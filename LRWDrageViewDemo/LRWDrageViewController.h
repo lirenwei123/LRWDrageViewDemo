787e023657ed2cae138180381a7ad4e8a0e49463
Capital <303624150@qq.com>
2016-11-18 18:20:43 +0800

    这是一个抽屉效果

---
+++ LRWDrageViewDemo/LRWDrageViewController.h
@@ -0,0 +1,17 @@
+//
+//  LRWDrageViewController.h
+//  LRWDrageViewDemo
+//
+//  Created by rwli on 16/11/18.
+//  Copyright © 2016年 companyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LRWDrageViewController : UIViewController
+@property (strong,nonatomic)UIView *leftV;
+@property (strong,nonatomic)UIView *midleV;
+@property (strong,nonatomic)UIView  *rightV;
+
+@end
+
---
+++ LRWDrageViewDemo/LRWDrageViewController.m
@@ -0,0 +1,95 @@
+//
+//  LRWDrageViewController.m
+//  LRWDrageViewDemo
+//
+//  Created by rwli on 16/11/18.
+//  Copyright © 2016年 companyName. All rights reserved.
+//
+
+#import "LRWDrageViewController.h"
+#import "objcBlockbyLRW.h"
+
+@interface LRWDrageViewController ()
+
+
+@end
+
+@implementation LRWDrageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupUI];
+    
+}
+
+-(void)setupUI{
+    UIColor *red =kRedColor;
+    UIColor *green =kGreenColor;
+    UIColor *blue =kBlueColor;
+    NSArray *colorArry =[NSArray arrayWithObjects:red,green,blue, nil];
+    for (int i=0; i<3; i++) {
+        UIView *view =[[UIView alloc]initWithFrame:self.view.bounds];
+        view.backgroundColor =colorArry[i];
+        [self.view insertSubview:view atIndex:i];
+        switch (i) {
+            case 0:
+                self.leftV=view;
+                break;
+            case 1:
+                self.rightV=view;
+                break;
+            case 2:
+                self.midleV=view;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    UIPanGestureRecognizer *pan =[[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(panClick:)];
+    [self.midleV addGestureRecognizer:pan];
+
+}
+
+-(void)panClick:(UIPanGestureRecognizer*)sender{
+    //move view
+    CGPoint point =[sender translationInView:self.view];
+       self.midleV.frame=[self framWithoffset_x:point.x];
+    [sender setTranslation:CGPointZero inView:self.midleV];
+    
+    //手势结束自动移动
+    if (sender.state ==UIGestureRecognizerStateEnded) {
+    #define off_x self.midleV.frame.origin.x
+        if (off_x>kScreenWidth*0.5) {
+            self.midleV.frame =[self framWithoffset_x:kScreenWidth*0.8-off_x];
+        }else if(off_x<-kScreenWidth*0.5){
+            self.midleV.frame=[self framWithoffset_x:-kScreenWidth*0.8-off_x];
+        }else{
+            self.midleV.frame=[self framWithoffset_x:-off_x];
+        }
+    }
+    
+}
+
+-(CGRect)framWithoffset_x:(CGFloat)x{
+    CGRect frame =self.midleV.frame;
+    if (frame.origin.x>0) {
+        self.rightV.hidden =YES;
+    }else if(frame.origin.x<0){
+        self.rightV.hidden=NO;
+    }
+
+    frame.origin.x +=x;
+    frame.origin.y= fabs(frame.origin.x/kScreenWidth*kScreenHeight*0.2);
+    frame.size.height= kScreenHeight -2*frame.origin.y;
+    return frame;
+}
+
+
+
+
+
+
+
+
+@end
